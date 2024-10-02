7f1d79278bc41f41ff4eb71af95f7695d18c9976
ruivergani <ruiverganineto@gmail.com>
2024-10-02 04:35:25 -0300

    Footer details desktop and mobile done

--- content/themes/assessment/assets/src/js/Components/Footer/index.js
+++ content/themes/assessment/assets/src/js/Components/Footer/index.js
@@ -6,12 +6,21 @@ import {
   FooterIcon,
   FooterTextCTA,
   FooterContentCTA,
-  FooterButtonCTA
+  FooterButtonCTA,
+  FooterCopy,
+  FooterMessage,
+  FooterDetails,
+  FooterContact,
+  FooterBottomContainer,
+  FooterSocials
  } from './styles';
-import {
-
-} from './styles';
-import { EnvelopeSimpleOpen } from 'phosphor-react';
+import { EnvelopeSimpleOpen, PhoneOutgoing, EnvelopeSimple} from 'phosphor-react';
+// Images
+import Iconfacebook from '../../../../images/logos/facebook.svg';
+import IconTwitter from '../../../../images/logos/twitter.svg';
+import IconInstagram from '../../../../images/logos/instagram.svg';
+import IconLinkedIn from '../../../../images/logos/linkedin.svg';
+import IconYouTube from '../../../../images/logos/youtube.svg';
 
 const Footer = () => {
   return (
@@ -33,7 +42,79 @@ const Footer = () => {
           </FooterContainerCTA>
         </FooterCTA>
       <FooterBottom>
-        Footer here
+        <FooterBottomContainer>
+          <div>
+
+          </div>
+          <FooterDetails>
+            <FooterSocials>
+              <p>Check our social media</p>
+              <ul>
+                <li>
+                  <a href="#" rel="noreferrer nofollow" target="_blank" title="Instagram LawnStarter">
+                    <picture>
+                      <img src={IconInstagram} alt="Instagram LawnStarter" title="Instagram LawnStarter" />
+                    </picture>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" rel="noreferrer nofollow" target="_blank" title="LinkedIn LawnStarter">
+                    <picture>
+                      <img src={IconLinkedIn} alt="LinkedIn LawnStarter" title="LinkedIn LawnStarter" />
+                    </picture>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" rel="noreferrer nofollow" target="_blank" title="Facebook LawnStarter">
+                    <picture>
+                      <img src={Iconfacebook} alt="Facebook LawnStarter" title="Facebook LawnStarter" />
+                    </picture>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" rel="noreferrer nofollow" target="_blank" title="Youtube LawnStarter">
+                    <picture>
+                      <img src={IconYouTube} alt="Youtube LawnStarter" title="Youtube LawnStarter" />
+                    </picture>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" rel="noreferrer nofollow" target="_blank" title="Twitter LawnStarter">
+                    <picture>
+                      <img src={IconTwitter} alt="Twitter LawnStarter" title="Youtube LawnStarter" />
+                    </picture>
+                  </a>
+                </li>
+              </ul>
+            </FooterSocials>
+            <FooterContact>
+              <h3>Contact Information</h3>
+              <p>Need some help?</p>
+              <span>Feel free to message us here</span>
+              <ul>
+                <li>
+                  <a href="mailto:enquires@lawnstartersupport.com">
+                    <EnvelopeSimple size={24} weight="duotone" />
+                    <p>enquires@lawnstartersupport.com</p>
+                  </a>
+                </li>
+                <li>
+                  <a href="tel:866‑822‑1766">
+                    <PhoneOutgoing size={24} weight="duotone" />
+                    <p>866‑822‑1766</p>
+                  </a>
+                </li>
+              </ul>
+            </FooterContact>
+          </FooterDetails>
+          <FooterMessage>
+            <p><strong>LawnStarter</strong> brings you the best lawn services, all at the click of button. </p>
+          </FooterMessage>
+          <FooterCopy>
+            <p>© 2024 - By submitting my contact information, I agree that LawnStarter or its representatives may contact me by email, phone, or SMS at the email address or phone number I provide, including for marketing purposes and by automatic dialing systems. I also agree to LawnStarter’s terms of service and privacy policy.</p>
+            <span>Developed by: <strong>Rui Vergani Neto</strong></span>
+          </FooterCopy>
+        </FooterBottomContainer>
       </FooterBottom>
     </FooterComponent>
   );
--- content/themes/assessment/assets/src/js/Components/Footer/styles.js
+++ content/themes/assessment/assets/src/js/Components/Footer/styles.js
@@ -137,5 +137,233 @@ export const FooterButtonCTA = styled.button`
 export const FooterBottom = styled.footer`
   background-color: #F3F3F5;
   padding-top: 65px;
+`;
+export const FooterBottomContainer = styled.div`
+  width: 100%;
+  max-width: 1260px;
+  margin: 0 auto;
+  padding: 0;
 
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  flex-direction: column;
+
+  @media (max-width: 1400px) {
+    padding: 0px 25px;
+  }
+  @media (max-width: 576px) {
+    padding: 0px 15px;
+  }
+`;
+export const FooterDetails = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  border-top: 1px solid rgba(208, 207, 214, 0.50);
+  padding: 44px 0px;
+
+  @media(max-width: 991px){
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    gap: 30px;
+  }
+
+`;
+export const FooterSocials = styled.div`
+  display: flex;
+  align-items: flex-start;
+  gap: 28px;
+  flex-direction: column;
+  p{
+    color: #000;
+    font-family: "Roboto", sans-serif;
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 150%;
+  }
+  ul{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 16px;
+    li{
+      a{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 56px;
+        height: 56px;
+        padding: 15.69px 16px 16.31px 16px;
+        border-radius: 8px;
+        border: 1px solid #D0CFD6;
+        transition: all .3s;
+        overflow: hidden;
+        &:hover{
+          picture{
+            opacity: 0;
+            img{
+              transform: translateY(100%);
+            }
+          }
+        }
+        picture{
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          transition: all .3s;
+          opacity: 1;
+          img{
+            display: block;
+            max-width: 100%;
+            transition: all .3s;
+            width: 24px;
+            height: 24px;
+          }
+        }
+      }
+    }
+  }
+  @media(max-width: 991px){
+    gap: 20px;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+  }
+  @media(max-width: 576px){
+    ul{
+      gap: 10px;
+      align-items: center;
+      justify-content: center;
+      text-align: center;
+      flex-direction: column;
+    }
+  }
+`;
+export const FooterContact = styled.div`
+  h3, p, span{
+    font-family: "Roboto", sans-serif;
+    font-size: 16px;
+    line-height: 25.6px;
+  }
+  h3{
+    color: #000;
+    font-weight: 700;
+    margin-bottom: 8px;
+  }
+  p{
+    font-weight: 500;
+    margin-bottom: 10px;
+  }
+  span{
+    color: #433E59;
+    font-size: 14px;
+  }
+  ul{
+    margin-top: 20px;
+    display: flex;
+    align-items: flex-start;
+    justify-content: center;
+    flex-direction: column;
+    gap: 10px;
+    li{
+      a{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 15px;
+        svg{
+          color: #067042;
+        }
+        p{
+          color: rgba(20, 13, 48, 0.70);
+          font-family: "Roboto", sans-serif;
+          font-size: 15px;
+          font-weight: 600;
+          line-height: 24px;
+          margin-bottom: 0px;
+          transition: all .3s;
+        }
+        &:hover{
+          p{
+            color: #000;
+          }
+        }
+      }
+    }
+  }
+  @media(max-width: 991px){
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    ul{
+      align-items: center;
+      justify-content: center;
+      text-align: center;
+    }
+  }
+`;
+export const FooterMessage = styled.div`
+  padding: 56px 0px;
+  width: 100%;
+  border-top: 1px solid rgba(208, 207, 214, 0.50);
+  border-bottom: 1px solid rgba(208, 207, 214, 0.50);
+  p{
+    width: 100%;
+    max-width: 320px;
+    color: #067042;
+    font-family: "Roboto", sans-serif;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 160%;
+    strong{
+      font-weight: 600;
+    }
+  }
+  @media (max-width: 991px){
+    padding: 46px 0px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    p{
+      text-align: center;
+    }
+  }
+  @media (max-width: 576px){
+    padding: 30px 0px;
+  }
+`;
+export const FooterCopy = styled.div`
+  width: 100%;
+  padding: 30px 0px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  p, span, strong{
+    font-family: "Roboto", sans-serif;
+    font-size: 12px;
+    line-height: 150%;
+  }
+  p, span{
+    font-weight: 400;
+  }
+  p{
+    width: 100%;
+    max-width: 908px;
+    color: #433E59;
+  }
+  span{
+    color: #000;
+  }
+  @media (max-width: 991px){
+    flex-direction: column;
+    gap: 20px;
+    p{
+      text-align: center;
+    }
+  }
 `;
